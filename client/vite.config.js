d824cc48318dff6c1185c2f56b46b55afa223fc0
Poornima Paneru <poornimapaneru3@gmail.com>
2026-09-11 15:23:28 +0530

    Allow Render host in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['rewear-frontend-r55a.onrender.com'],
+  },
 })
