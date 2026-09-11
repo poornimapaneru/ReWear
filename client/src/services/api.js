a8cac7d690e40eb90424f1ff633945fb9bd04ac2
Poornima Paneru <poornimapaneru3@gmail.com>
2026-09-11 13:22:27 +0530

    Update frontend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://rewear-1-yskd.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
